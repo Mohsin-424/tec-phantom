6572b999e3a200ba511a5d61fe97fa0d90d64277
Mohsin-424 <mohsinpu24@gmail.com>
2024-08-19 10:01:59 +0500

    Tuples in Python

---
+++ tuple.py
@@ -0,0 +1,69 @@
+# tup = (1 , 2 ,40, 3, 4, 5
+
+# print(tup)
+# print(type(tup))
+
+# # tup.sort() # cannot be sorted
+# tup1= ("hello World",)
+
+
+
+# print(type(tup))
+# print(tup)
+
+# tup = (1 , 2 ,40, 3, 4, 5)
+# tup1 = ( "hello World","hello", "world")
+# tup2 = (True , False ,1,0)
+# tup3 = ('q' , 'w' , 'e','r','t','t')
+# print(type(tup))
+# print(type(tup1))
+# print(type(tup2))
+# print(type(tup3))
+
+
+# Tuple Constructors --------------------------------
+
+# Creating a tuple with single element
+# tup = ("Hello ",7,5,34,4)
+# print(tup)
+# print(list(tup))
+# print(len(tup))
+# print(tup[::3])
+
+# y = list(tup)
+
+# y.append("World")
+# print(y)
+# z= tuple(y)
+# print(z)
+# print(type(z))
+
+# # Creating a tuple with list
+# slap = tuple[1,2,3,4,5,5,6,7,8,89,0]
+# print(slap)
+
+# tup1 = ("   Hello   " )
+# tup2  = ("     World       " )
+# tup3 = ("   !    ")
+# tup = tup1 + tup2 + tup3
+# print(tup)
+
+
+
+tup = ( 1,2,3,4,int('5'),6,7)
+y = list(tup)
+print(y)
+
+# y.insert(0 , " Hello World!")
+# print(y)
+# tup = tuple(y)
+# print(tup)
+
+y.remove((5))
+del y[0]
+
+print(y)
+
+
+
+
