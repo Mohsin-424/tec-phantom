cdf491f4a6a430c430203a53280b08bd2de80d84
Mohsin-424 <mohsinpu24@gmail.com>
2024-08-19 09:11:51 +0500

    Python Lists

---
+++ list.py
@@ -0,0 +1,18 @@
+ls1 = [  8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16]
+ls2 = [ 1, 2, 3, 4, 5, 6, 7, 8]
+
+ls1.append(2)
+# ls1.add([3])
+
+# print(ls1)
+# ls1.pop()
+# print(ls1)
+
+# ls1.extend
+
+# print(ls1[1:-4])
+# print(ls1[1:1:1])
+
+ls1.insert(0,3)
+print(ls1)
+
