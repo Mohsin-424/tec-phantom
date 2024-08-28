44986d4fe11819db548c838b65952c82bbff648f
Mohsin-424 <mohsinpu24@gmail.com>
2024-08-29 00:23:16 +0500

    Python Project

--- Projects/Projecct1.py
+++ Projects/Projecct1.py
@@ -1,9 +1,16 @@
-from tkinter import ttk , Tk , PhotoImage , Canvas , filedialog 
 
-root = Tk()
+# OpenCV Basics 
+import cv2
+
+# image = cv2.imread("Projects/3.jpg")
+image = cv2.imread("Projects/1.png")
 
 
-# filename = filedialog.askopenfilename()
-filename = filedialog.asksaveasfile()
-print(filename)
-root.mainloop()
+# image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+image = cv2.bitwise_not(image)
+
+assert image is not None, "Failed to load image"
+cv2.imshow("   This is processes image" , image)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
