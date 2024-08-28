1fc654f43fadd9c590aa52f38130b367de52e058
Mohsin-424 <mohsinpu24@gmail.com>
2024-08-28 12:25:28 +0500

    Python Bootcamp

--- Web-Scrapping.py
+++ Web-Scrapping.py
@@ -1,11 +1,16 @@
 import requests
 import bs4
-import requests
+ 
 
 
 # result = requests.get('https://mohsin-424.github.io/ee/')
 # result = requests.get('https://creativecommons.org/')
-result = requests.get('https://sci-hub.se/')
+# result = requests.get('https://sci-hub.se/')
+# result = requests.get('https://moviesmod.monster/')
+# result = requests.get('https://github.com/Mohsin-424')
+result = requests.get('https://en.wikipedia.org/wiki/Elon_Musk')
+# result = requests.get('https://medium.com/geekculture/web-scraping-with-python-a-complete-step-by-step-guide-code-5174e52340ea')
+
 print(type(result))
 print(result)
 sop = bs4.BeautifulSoup(result.text , "lxml")
@@ -17,7 +22,14 @@ print(sop)
 # site_para = sop.select('.skip-to-content')
 # site_para = sop.select('footer')
 site_para = sop.select('header')
+site_para = type(sop.select('header'))
 print(site_para)
+
+
+
+for item in sop.select('header'):
+    print(item.text)
+
 
 # site_para[1].get_text()
 
@@ -53,3 +65,19 @@ print(site_para)
 # else:
 #     print(f"Failed to fetch the website. Status code: {result.status_code}")
 
+
+
+
+import csv
+# data to be saved
+data = [
+['Jay', 'Dominic', 25],
+['Justin', 'Seam', 30],
+['Bob', 'Lans', 40]
+]
+# open a file for writing
+with open('data.csv', mode='w', newline='') as file:
+# create a csv writer object
+            writer = csv.writer(file)
+# write the data to the file
+writer.writerows(data)
