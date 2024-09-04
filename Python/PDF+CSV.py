3c2bf70385df131beb4dfc5c64f4b9f81f3062a5
Mohsin-424 <mohsinpu24@gmail.com>
2024-09-04 10:43:55 +0500

    Python Bootcamp

--- Python/PDF+CSV.py
+++ Python/PDF+CSV.py
@@ -1,3 +1,14 @@
-#...................... pip install Openpyxl for  dealing with Excel CSV files in Python ................
+#...................... pip install Openpyxl-->for  dealing with Excel CSV files in Python ................
+
+import csv
+# Open the File
+data = open("data.csv" , encoding = "utf-8")
+# CSV Reader
+csv_data = csv.reader(data)
+# Reformat it into a Python Object  List of Lists
+data_lines = list(csv_data)
+print(data_lines)
+  
 
 
+import pyPDF2
