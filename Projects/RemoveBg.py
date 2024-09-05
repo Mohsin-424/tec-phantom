0edd9c9514d97c90f2107dfcd65d52c159d69e40
Mohsin-424 <mohsinpu24@gmail.com>
2024-09-05 10:12:26 +0500

    Python Bootcamp

--- Projects/RemoveBg.py
+++ Projects/RemoveBg.py
@@ -1,5 +1,12 @@
-import numpy as np
 import removebg   # We can make API of Removebg on my own
-import rembg
-
-
+from rembg import remove
+import easygui
+from PIL import Image
+# Open and Save File
+inputFile = easygui.fileopenbox("Select ay File.....")
+Outfile =   easygui.filesavebox("Save File")
+# Take image as input
+input = Image.open(inputFile)
+output = remove(input)
+# Save the Output
+output.save(Outfile)
